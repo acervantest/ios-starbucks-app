39618f25519872d039f835e05a2a9616c6d0229d
Alejandro <acervantest83@gmail.com>
2024-04-24 22:32:58 -0700

    Initial No Storyboard set up

---
+++ Starbucks/Starbucks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Starbucks
+//
+//  Created by Alejandro Cervantes on 2024-04-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
