6727c994db702c9aec8bbfde789f41c177395797
Alejandro <acervantest83@gmail.com>
2024-04-26 12:33:22 -0700

    Add Get Rid Of table View

--- Starbucks/Starbucks/HomeViewController.swift
+++ Starbucks/Starbucks/HomeViewController.swift
@@ -12,8 +12,6 @@ class HomeViewController: StarbucksViewController {
     let homeHeader = HomeHeaderView()
     var homeHeaderTopConstraint: NSLayoutConstraint?
     
-    let tableView = UITableView()
-    
     let cellId = "cellId"
     let tiles = ["Star Balance", "Bonus Stars", "Try These", "Welcome Back", "Uplifting"]
 
@@ -29,28 +27,16 @@ extension HomeViewController {
     private func setUp() {
         style()
         layout()
-        setupTableView()
     }
  
     func style() {
         homeHeader.translatesAutoresizingMaskIntoConstraints = false
         homeHeader.backgroundColor = .systemMint
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    func setupTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-        tableView.tableFooterView = UIView()
     }
     
     func layout() {
         
         view.addSubview(homeHeader)
-        view.addSubview(tableView)
         
         homeHeaderTopConstraint = homeHeader.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
         
@@ -59,53 +45,25 @@ extension HomeViewController {
             homeHeaderTopConstraint!,
             homeHeader.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             homeHeader.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            // TABLE VIEW
-            tableView.topAnchor.constraint(equalTo: homeHeader.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
 }
 
-// MARK: - TableView Data Source
-extension HomeViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        
-        cell.textLabel?.text = tiles[indexPath.row]
-        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tiles.count
-    }
-}
+// MARK: - Animating Scroll View
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let y = scrollView.contentOffset.y
+//        
+//        let swipingDown = y <= 0
+//        let shouldSnap = y > 30
+//        let labelHeight = homeHeader.greeting.frame.height + 16 // label + spacer
+//        
+//        UIView.animate(withDuration: 0.3) {
+//            self.homeHeader.greeting.alpha = swipingDown ? 1.0 : 0.0
+//        }
+//        
+//        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, animations: {
+//            self.homeHeaderTopConstraint?.constant = shouldSnap ? -labelHeight : 0
+//            self.view.layoutIfNeeded()
+//        })
+//    }
 
-// MARK: - TableView Delegate
-extension HomeViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 300
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let y = scrollView.contentOffset.y
-        
-        let swipingDown = y <= 0
-        let shouldSnap = y > 30
-        let labelHeight = homeHeader.greeting.frame.height + 16 // label + spacer
-        
-        UIView.animate(withDuration: 0.3) {
-            self.homeHeader.greeting.alpha = swipingDown ? 1.0 : 0.0
-        }
-        
-        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, animations: {
-            self.homeHeaderTopConstraint?.constant = shouldSnap ? -labelHeight : 0
-            self.view.layoutIfNeeded()
-        })
-    }
-}
